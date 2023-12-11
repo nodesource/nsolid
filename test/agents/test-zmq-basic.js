1415c8bb0859a17af55646ef2c3d23035e07b199
Santiago Gimeno <santiago.gimeno@gmail.com>
2023-12-11 21:37:36 +0100

    WPI

---
+++ test/agents/test-zmq-basic.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const common = require('../common');
+
+const { before, describe, it } = require('node:test');
+
+describe('basic boostrap', () => {
+  before(async () => {
+  });
+});
