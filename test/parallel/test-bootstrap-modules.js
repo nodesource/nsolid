4b0d91a7e42f3516ecef3f6c2e071431d03c3680
Trevor Norris <trev.norris@gmail.com>
2024-01-08 15:31:59 -0700

    test: fix bootstrap modules

--- test/parallel/test-bootstrap-modules.js
+++ test/parallel/test-bootstrap-modules.js
@@ -97,17 +97,27 @@ expected.beforePreExec = new Set([
   'NativeModule internal/modules/package_json_reader',
   'Internal Binding module_wrap',
   'NativeModule internal/modules/cjs/loader',
+  'Internal Binding nsolid_api',
+  'Internal Binding worker',
+  'NativeModule internal/nsolid_module',
 ]);
 
 expected.atRunTime = new Set([
   'Internal Binding wasm_web_api',
-  'Internal Binding worker',
   'NativeModule internal/modules/run_main',
   'NativeModule internal/net',
   'NativeModule internal/dns/utils',
   'NativeModule internal/process/pre_execution',
   'NativeModule internal/vm/module',
   'NativeModule internal/modules/esm/utils',
+  'Internal Binding nsolid_statsd_agent',
+  'Internal Binding nsolid_zmq_agent',
+  'NativeModule internal/agents/statsd/lib/agent',
+  'NativeModule internal/agents/statsd/lib/nsolid',
+  'NativeModule internal/agents/zmq/lib/agent',
+  'NativeModule internal/agents/zmq/lib/nsolid',
+  'NativeModule internal/nsolid_loader',
+  'NativeModule internal/nsolid_promise_tracking',
 ]);
 
 if (common.isMainThread) {
