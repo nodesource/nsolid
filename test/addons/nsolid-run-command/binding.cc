0995d5f12a4037280f65f6b6cfe128fc7f3a15ee
Santiago Gimeno <santiago.gimeno@gmail.com>
2024-01-30 10:50:18 +0100

    WIP

--- test/addons/nsolid-run-command/binding.cc
+++ test/addons/nsolid-run-command/binding.cc
@@ -1,22 +1,15 @@
 #include <node.h>
 #include <uv.h>
 #include <v8.h>
-#include <nsolid/nsolid_api.h>
+#include <nsolid.h>
 
 #include <assert.h>
-
-using node::nsolid::EnvInst;
-using node::nsolid::EnvList;
 
 using v8::FunctionCallbackInfo;
 using v8::Value;
 
 std::atomic<int> interrupt_cb_cntr = { 0 };
 std::atomic<int> interrupt_only_cb_cntr = { 0 };
-
-static void at_exit(void*) {
-  assert(EnvList::Inst()->env_map_size() == 1);
-}
 
 static void interrupt_cb(node::nsolid::SharedEnvInst) {
   ++interrupt_cb_cntr;
@@ -59,9 +52,4 @@ NODE_MODULE_INIT(/* exports, module, context */) {
   NODE_SET_METHOD(exports, "getInterruptOnlyCntr", GetInterruptOnlyCntr);
   NODE_SET_METHOD(exports, "runInterrupt", RunInterrupt);
   NODE_SET_METHOD(exports, "runInterruptOnly", RunInterruptOnly);
-  // While NODE_MODULE_INIT will run for every Worker, the first execution
-  // won't run in parallel with another. So this won't cause a race condition.
-  if (EnvInst::GetCurrent(context)->thread_id() == 0) {
-    node::AtExit(node::GetCurrentEnvironment(context), at_exit, nullptr);
-  }
 }
