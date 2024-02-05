a8757c7a18e4373b929d5ba2f5606906dd7edf1f
Santiago Gimeno <santiago.gimeno@gmail.com>
2024-02-05 13:17:05 +0100

    WIP

--- vcbuild.bat
+++ vcbuild.bat
@@ -646,7 +646,7 @@ rd /s /q test\common\nsolid-zmq-agent\node_modules
 :: installing the modules
 setlocal
 set npm_config_nodedir=%~dp0
-%npm_exe% install zeromq@5 base85 --prefix "%~dp0test\common\nsolid-zmq-agent" --no-save --no-package-lock
+%npm_exe% install zeromq@5 zmq-zap --prefix "%~dp0test\common\nsolid-zmq-agent" --no-save --no-package-lock
 %npm_exe% run build:libzmq --prefix "%~dp0test\common\nsolid-zmq-agent\node_modules\zeromq"
 if errorlevel 1 exit /b 1
 endlocal
