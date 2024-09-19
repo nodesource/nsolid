173644eec631b39aa6fd741afa21d998589c70bf
Trevor Norris <trev.norris@gmail.com>
2024-09-19 17:47:36 -0600

    build: add back format-md

--- vcbuild.bat
+++ vcbuild.bat
@@ -794,7 +794,21 @@ for /D %%D IN (doc\*) do (
     set "lint_md_files="%%F" !lint_md_files!"
   )
 )
-%node_exe% tools\lint-md.mjs -q -f %lint_md_files%
+%node_exe% tools\lint-md\lint-md.mjs %lint_md_files%
+ENDLOCAL
+goto format-md
+
+:format-md
+if not defined format_md goto exit
+echo Running Markdown formatter on docs...
+SETLOCAL ENABLEDELAYEDEXPANSION
+set lint_md_files=
+for /D %%D IN (doc\*) do (
+  for %%F IN (%%D\*.md) do (
+    set "lint_md_files="%%F" !lint_md_files!"
+  )
+)
+%node_exe% tools\lint-md\lint-md.mjs --format %lint_md_files%
 ENDLOCAL
 goto exit
 
