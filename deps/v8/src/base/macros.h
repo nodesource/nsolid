5677515fac48bd55a34de6a754d30b950f2b776d
Trevor Norris <trev.norris@gmail.com>
2024-10-03 17:47:36 -0600

    v8: fix clang build issue
    See: https://chromium-review.googlesource.com/c/v8/v8/+/5872655

--- deps/v8/src/base/macros.h
+++ deps/v8/src/base/macros.h
@@ -173,7 +173,8 @@ namespace base {
 // base::is_trivially_copyable will differ for these cases.
 template <typename T>
 struct is_trivially_copyable {
-#if V8_CC_MSVC || (__GNUC__ == 12 && __GNUC_MINOR__ <= 2)
+#if V8_CC_MSVC || (__GNUC__ == 12 && __GNUC_MINOR__ <= 2) || \
+  (defined(__clang__) && __clang_major__ <= 17)
   // Unfortunately, MSVC 2015 is broken in that std::is_trivially_copyable can
   // be false even though it should be true according to the standard.
   // (status at 2018-02-26, observed on the msvc waterfall bot).
@@ -186,6 +187,8 @@ struct is_trivially_copyable {
   // distributions, so the same polyfill is also used.
   // See
   // https://gcc.gnu.org/git/?p=gcc.git;a=commitdiff;h=aeba3e009b0abfccaf01797556445dbf891cc8dc
+  //
+  // Also observed with an eclectic range of clang++ releases.
   static constexpr bool value =
       // Copy constructor is trivial or deleted.
       (std::is_trivially_copy_constructible<T>::value ||
