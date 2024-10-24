47b8b39e693f912236f304390098440aa46847d1
Santiago Gimeno <santiago.gimeno@gmail.com>
2024-10-24 16:38:20 +0200

    agents: add GrpcClient implementation
    The static `GrpcClient` namespace exposes multiple methods implementing
    part of the NSolidService rpc's.
    
    - The `MakeChannel()` methods, which accepts `OtlpGrpcClientOptions` as
      a param, which allows creating a gRPC channel.
    - The `MakeClientContext()` method which ºsets up a gRPC client context with
      metadata: `nsolid-agent-id: agent_id` and `nsolid-saas-token: token`.
    - The `MakeNSolidServiceStub` method which creates a stub for the N|Solid service
      to facilitate communication with the N|Solid Console.
    - The `DelegateAsyncExport()` methods which perform all the unary rpc
      calls defined in `NSolidService`.
    
    PR-URL: https://github.com/nodesource/nsolid/pull/203
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

---
+++ agents/grpc/src/grpc_client.h
@@ -0,0 +1,131 @@
+#ifndef AGENTS_GRPC_SRC_GRPC_CLIENT_H_
+#define AGENTS_GRPC_SRC_GRPC_CLIENT_H_
+
+#include "../../src/profile_collector.h"
+#include "./proto/nsolid_service.grpc.pb.h"
+#include "grpcpp/grpcpp.h"
+#include "opentelemetry/version.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace exporter {
+namespace otlp {
+struct OtlpGrpcClientOptions;
+}
+}
+OPENTELEMETRY_END_NAMESPACE
+
+using opentelemetry::v1::exporter::otlp::OtlpGrpcClientOptions;
+
+namespace node {
+namespace nsolid {
+namespace grpc {
+
+class GrpcClient {
+ public:
+  /**
+   * Create gRPC channel.
+   */
+  static std::shared_ptr<::grpc::Channel>
+    MakeChannel(const OtlpGrpcClientOptions& options);
+
+  /**
+   * Create gRPC client context to call RPC.
+   */
+  static std::unique_ptr<::grpc::ClientContext>
+    MakeClientContext(const std::string& agent_id, const std::string& saas);
+
+  /**
+   * Create N|Solid service stub to communicate with the N|Solid Console.
+   */
+  static std::unique_ptr<grpcagent::NSolidService::StubInterface>
+    MakeNSolidServiceStub(const OtlpGrpcClientOptions& options);
+
+  static int DelegateAsyncExport(
+    grpcagent::NSolidService::StubInterface* stub,
+    std::unique_ptr<::grpc::ClientContext>&& context,
+    std::unique_ptr<google::protobuf::Arena>&& arena,
+    grpcagent::BlockedLoopEvent&& event,
+    std::function<bool(::grpc::Status,
+                        std::unique_ptr<google::protobuf::Arena> &&,
+                        const grpcagent::BlockedLoopEvent&,
+                        grpcagent::EventResponse*)>&& result_callback) noexcept;
+
+
+  static int DelegateAsyncExport(
+    grpcagent::NSolidService::StubInterface* stub,
+    std::unique_ptr<::grpc::ClientContext>&& context,
+    std::unique_ptr<google::protobuf::Arena>&& arena,
+    grpcagent::UnblockedLoopEvent&& event,
+    std::function<bool(::grpc::Status,
+                        std::unique_ptr<google::protobuf::Arena> &&,
+                        const grpcagent::UnblockedLoopEvent&,
+                        grpcagent::EventResponse*)>&& result_callback) noexcept;
+
+  static int DelegateAsyncExport(
+    grpcagent::NSolidService::StubInterface* stub,
+    std::unique_ptr<::grpc::ClientContext>&& context,
+    std::unique_ptr<google::protobuf::Arena>&& arena,
+    grpcagent::ExitEvent&& event,
+    std::function<bool(::grpc::Status,
+                        std::unique_ptr<google::protobuf::Arena> &&,
+                        const grpcagent::ExitEvent&,
+                        grpcagent::EventResponse*)>&& result_callback) noexcept;
+
+
+  static int DelegateAsyncExport(
+    grpcagent::NSolidService::StubInterface* stub,
+    std::unique_ptr<::grpc::ClientContext>&& context,
+    std::unique_ptr<google::protobuf::Arena>&& arena,
+    grpcagent::InfoEvent&& event,
+    std::function<bool(::grpc::Status,
+                        std::unique_ptr<google::protobuf::Arena> &&,
+                        const grpcagent::InfoEvent&,
+                        grpcagent::EventResponse*)>&& result_callback) noexcept;
+
+
+  static int DelegateAsyncExport(
+    grpcagent::NSolidService::StubInterface* stub,
+    std::unique_ptr<::grpc::ClientContext>&& context,
+    std::unique_ptr<google::protobuf::Arena>&& arena,
+    grpcagent::MetricsEvent&& event,
+    std::function<bool(::grpc::Status,
+                        std::unique_ptr<google::protobuf::Arena> &&,
+                        const grpcagent::MetricsEvent&,
+                        grpcagent::EventResponse*)>&& result_callback) noexcept;
+
+  static int DelegateAsyncExport(
+    grpcagent::NSolidService::StubInterface* stub,
+    std::unique_ptr<::grpc::ClientContext>&& context,
+    std::unique_ptr<google::protobuf::Arena>&& arena,
+    grpcagent::PackagesEvent&& event,
+    std::function<bool(::grpc::Status,
+                        std::unique_ptr<google::protobuf::Arena> &&,
+                        const grpcagent::PackagesEvent&,
+                        grpcagent::EventResponse*)>&& result_callback) noexcept;
+
+  static int DelegateAsyncExport(
+    grpcagent::NSolidService::StubInterface* stub,
+    std::unique_ptr<::grpc::ClientContext>&& context,
+    std::unique_ptr<google::protobuf::Arena>&& arena,
+    grpcagent::ReconfigureEvent&& event,
+    std::function<bool(::grpc::Status,
+                        std::unique_ptr<google::protobuf::Arena> &&,
+                        const grpcagent::ReconfigureEvent&,
+                        grpcagent::EventResponse*)>&& result_callback) noexcept;
+
+  static int DelegateAsyncExport(
+    grpcagent::NSolidService::StubInterface* stub,
+    std::unique_ptr<::grpc::ClientContext>&& context,
+    std::unique_ptr<google::protobuf::Arena>&& arena,
+    grpcagent::StartupTimesEvent&& event,
+    std::function<bool(::grpc::Status,
+                        std::unique_ptr<google::protobuf::Arena> &&,
+                        const grpcagent::StartupTimesEvent&,
+                        grpcagent::EventResponse*)>&& result_callback) noexcept;
+};
+
+}  // namespace grpc
+}  // namespace nsolid
+}  // namespace node
+
+#endif  // AGENTS_GRPC_SRC_GRPC_CLIENT_H_
