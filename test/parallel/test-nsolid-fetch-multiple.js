3d2da9eb0bcf28e1a213f71b772d2436f5b0713c
Santiago Gimeno <santiago.gimeno@gmail.com>
2024-10-28 18:37:02 +0100

    lib: add counter support for undici fetch()
    PR-URL: https://github.com/nodesource/nsolid/pull/204
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

---
+++ test/parallel/test-nsolid-fetch-multiple.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const common = require('../common');
+const assert = require('assert');
+const http = require('http');
+const nsolid = require('nsolid');
+
+const REQUESTS = 10;
+
+const server = http.createServer(common.mustCall((req, res) => {
+  if (req.url === '/1') {
+    setTimeout(() => {
+      res.end();
+    }, 1000);
+  } else {
+    res.end();
+  }
+}, REQUESTS));
+
+server.listen(0, '127.0.0.1', common.mustSucceed(async () => {
+  // Check that the fetch counters work correctly even with multiple requests
+  // in parallel.
+  const requests = [];
+  for (let i = 0; i < REQUESTS; i++) {
+    requests.push(fetch(`http://127.0.0.1:${server.address().port}/${i}`));
+  }
+
+  await Promise.all(requests);
+  assert.strictEqual(nsolid.traceStats.httpClientCount, REQUESTS);
+  assert.strictEqual(nsolid.traceStats.httpClientAbortCount, 0);
+  assert.strictEqual(nsolid.traceStats.httpServerCount, REQUESTS);
+  assert.strictEqual(nsolid.traceStats.httpServerAbortCount, 0);
+  // Wait for more than 3 secs for the percentiles to be updated
+  setTimeout(() => {
+    const metrics = require('nsolid').metrics();
+    // As all requests but one are fast, the median should be less than 200ms
+    // and the 99th percentile should be more than 1000ms due to the slow
+    // request.
+    assert.ok(metrics.httpClientMedian < 200);
+    assert.ok(metrics.httpClient99Ptile > 1000);
+    server.close();
+  }, 3500);
+}));
