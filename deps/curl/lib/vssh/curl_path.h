61c29d909e7bc9dfa3d8ade1b40d487700d65794
Santiago Gimeno <santiago.gimeno@gmail.com>
2024-11-14 08:58:04 +0100

    deps: update to libcurl 8.11.0
    PR-URL: https://github.com/nodesource/nsolid/pull/217
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>
    PR-URL: https://github.com/nodesource/nsolid/pull/225
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>

--- deps/curl/lib/vssh/curl_path.h
+++ deps/curl/lib/vssh/curl_path.h
@@ -28,22 +28,9 @@
 #include <curl/curl.h>
 #include "urldata.h"
 
-#ifdef WIN32
-#  undef  PATH_MAX
-#  define PATH_MAX MAX_PATH
-#  ifndef R_OK
-#    define R_OK 4
-#  endif
-#endif
-
-#ifndef PATH_MAX
-#define PATH_MAX 1024 /* just an extra precaution since there are systems that
-                         have their definition hidden well */
-#endif
-
 CURLcode Curl_getworkingpath(struct Curl_easy *data,
                              char *homedir,
                              char **path);
 
-CURLcode Curl_get_pathname(const char **cpp, char **path, char *homedir);
+CURLcode Curl_get_pathname(const char **cpp, char **path, const char *homedir);
 #endif /* HEADER_CURL_PATH_H */
