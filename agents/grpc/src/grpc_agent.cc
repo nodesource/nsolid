8e57724b26103a4ed4b49c2702d03705872f0325
Santiago Gimeno <santiago.gimeno@gmail.com>
2024-11-18 18:53:22 +0100

    agents: fix ExitEvent condition handling
    A couple of fixes here:
    - Both the `uv_mutex_t` and `uv_cond_t` should be initialized before
      defining the lambda.
    - Add a boolean variable to make sure we're not waiting forever in the
      case the lambda code is run before reaching the `uv_cond_wait()`.
    
    PR-URL: https://github.com/nodesource/nsolid/pull/222
    Reviewed-By: Trevor Norris <trev.norris@gmail.com>

--- agents/grpc/src/grpc_agent.cc
+++ agents/grpc/src/grpc_agent.cc
@@ -1593,24 +1593,29 @@ void GrpcAgent::send_exit() {
   auto context = GrpcClient::MakeClientContext(agent_id_, saas_);
   uv_cond_t cond;
   uv_mutex_t lock;
+  bool signaled = false;
+
+  uv_mutex_init(&lock);
+  uv_cond_init(&cond);
   GrpcClient::DelegateAsyncExport(
     nsolid_service_stub_.get(), std::move(context), std::move(arena),
     std::move(*exit_event),
-    [&lock, &cond](::grpc::Status,
+    [&lock, &cond, &signaled](::grpc::Status,
         std::unique_ptr<Arena> &&,
         const grpcagent::ExitEvent& event,
         grpcagent::EventResponse*) {
       uv_mutex_lock(&lock);
+      signaled = true;
       uv_cond_signal(&cond);
       uv_mutex_unlock(&lock);
       return true;
     });
 
   // wait for the exit event to be sent
-  uv_mutex_init(&lock);
-  uv_cond_init(&cond);
   uv_mutex_lock(&lock);
-  uv_cond_wait(&cond, &lock);
+  while (!signaled) {
+    uv_cond_wait(&cond, &lock);
+  }
   uv_mutex_unlock(&lock);
   uv_cond_destroy(&cond);
   uv_mutex_destroy(&lock);
