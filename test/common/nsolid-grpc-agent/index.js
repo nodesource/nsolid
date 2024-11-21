67d4f32c36408485b14a69b9c795038ee08270bb
Santiago Gimeno <santiago.gimeno@gmail.com>
2024-11-21 15:02:20 +0100

    test: get opentelemetry version from process
    PR-URL: https://github.com/nodesource/nsolid/pull/221
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>
    PR-URL: https://github.com/nodesource/nsolid/pull/245
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- test/common/nsolid-grpc-agent/index.js
+++ test/common/nsolid-grpc-agent/index.js
@@ -44,7 +44,7 @@ function checkResource(resource, agentId, config, metrics) {
   validateArray(resource.attributes, 'attributes');
 
   const expectedAttributes = {
-    'telemetry.sdk.version': '1.16.0',
+    'telemetry.sdk.version': process.versions.opentelemetry,
     'telemetry.sdk.language': 'cpp',
     'telemetry.sdk.name': 'opentelemetry',
     'service.instance.id': agentId,
