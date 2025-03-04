3aec43a0f628e1162f6756ba69e7584025b7ca44
Santiago Gimeno <santiago.gimeno@gmail.com>
2025-03-04 11:18:08 +0100

    lib,src,test: add http protocol version to spans
    Fixes: https://github.com/nodesource/nsolid-roadmap/issues/26
    PR-URL: https://github.com/nodesource/nsolid/pull/276
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>

--- test/addons/nsolid-tracing/test-http2-client-destroy-before-goaway.js
+++ test/addons/nsolid-tracing/test-http2-client-destroy-before-goaway.js
@@ -15,6 +15,7 @@ function setupTracesCheck(port, addresses) {
       attributes: {
         'http.method': 'GET',
         'http.url': `http://localhost:${port}/`,
+        'network.protocol.version': '2',
       },
       end_reason: binding.kSpanEndOk,
       name: 'HTTP GET',
@@ -30,6 +31,7 @@ function setupTracesCheck(port, addresses) {
           attributes: {
             'http.method': 'GET',
             'http.url': `http://localhost:${port}/`,
+            'network.protocol.version': '2',
           },
           end_reason: binding.kSpanEndOk,
           name: 'HTTP GET',
--- test/addons/nsolid-tracing/test-http2-server-session-destroy.js
+++ test/addons/nsolid-tracing/test-http2-server-session-destroy.js
@@ -15,6 +15,7 @@ function setupTracesCheck(port, addresses) {
       attributes: {
         'http.method': 'POST',
         'http.url': `http://localhost:${port}/`,
+        'network.protocol.version': '2',
       },
       end_reason: binding.kSpanEndOk,
       name: 'HTTP POST',
@@ -27,6 +28,7 @@ function setupTracesCheck(port, addresses) {
           attributes: {
             'http.method': 'POST',
             'http.url': `http://localhost:${port}/`,
+            'network.protocol.version': '2',
           },
           end_reason: binding.kSpanEndOk,
           name: 'HTTP POST',
