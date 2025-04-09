cf85b9fc27736fa34c53b2f328978110e69fad86
RafaelGSS <rafael.nunu@hotmail.com>
2025-04-09 10:31:32 -0300

    src: store hasEbpfSupport metadata in info.proto
    PR-URL: https://github.com/nodesource/nsolid/pull/290
    Refs: https://github.com/nodesource/ebpf-roadmap/issues/1
    Reviewed-By: Santiago Gimeno <santiago.gimeno@gmail.com>
    PR-URL: https://github.com/nodesource/nsolid/pull/359
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- agents/grpc/src/grpc_agent.cc
+++ agents/grpc/src/grpc_agent.cc
@@ -250,6 +250,10 @@ void PopulateInfoEvent(grpcagent::InfoEvent* info_event,
         (*body->mutable_versions())[key] = value.get<std::string>();
       }
     }
+
+    if (info.find("kernelVersion") != info.end()) {
+      body->set_kernelversion(info["kernelVersion"].get<uint32_t>());
+    }
   }
 }
 
