6813093ccbb0dc8dcfb13d4f04981b97abb53517
Santiago Gimeno <santiago.gimeno@gmail.com>
2025-05-07 09:11:13 +0200

    Revert "agents: fix cont profiling timestamp calculation"
    This reverts commit 9265d2faa37ee09f3b448a34ec8664022977a05d.
    
    PR-URL: https://github.com/nodesource/nsolid/pull/304
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>

--- test/common/nsolid-grpc-agent/server.mjs
+++ test/common/nsolid-grpc-agent/server.mjs
@@ -127,9 +127,8 @@ async function startServer(cb) {
         metadata: null,
         data: '',
         duration: null,
-        startTs: null,
-        endTs: null,
       };
+      call._my_data = '';
       call.on('data', (data) => {
         console.log('[ExportContinuousProfile] data', data.data.length);
         asset.common = data.common;
@@ -138,8 +137,6 @@ async function startServer(cb) {
         asset.data += data.data;
         if (data.complete) {
           asset.duration = data.duration;
-          asset.startTs = data.startTs;
-          asset.endTs = data.endTs;
         }
       });
       call.on('error', (err) => {
