eb73b40d2b36921a6e16c01277dbbba3654b75d1
Santiago Gimeno <santiago.gimeno@gmail.com>
2025-06-06 00:10:36 +0200

    deps: update grpc to 1.73.1
    PR-URL: https://github.com/nodesource/nsolid/pull/324
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- agents/grpc/src/proto/nsolid_service.grpc.pb.cc
+++ agents/grpc/src/proto/nsolid_service.grpc.pb.cc
@@ -19,6 +19,7 @@
 #include <grpcpp/server_context.h>
 #include <grpcpp/impl/service_type.h>
 #include <grpcpp/support/sync_stream.h>
+#include <grpcpp/ports_def.inc>
 namespace grpcagent {
 
 static const char* NSolidService_method_names[] = {
@@ -523,4 +524,5 @@ NSolidService::Service::~Service() {
 
 
 }  // namespace grpcagent
+#include <grpcpp/ports_undef.inc>
 
