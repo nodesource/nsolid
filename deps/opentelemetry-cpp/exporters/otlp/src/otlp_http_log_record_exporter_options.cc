d864541b46a6b2ec6b0f0b199d6078106b35683d
Santiago Gimeno <santiago.gimeno@gmail.com>
2025-07-22 18:00:11 +0200

    deps: update to opentelemetry 1.22.0
    PR-URL: https://github.com/nodesource/nsolid/pull/345
    Reviewed-By: Juan José Arboleda <soyjuanarbol@gmail.com>
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- deps/opentelemetry-cpp/exporters/otlp/src/otlp_http_log_record_exporter_options.cc
+++ deps/opentelemetry-cpp/exporters/otlp/src/otlp_http_log_record_exporter_options.cc
@@ -4,6 +4,7 @@
 #include "opentelemetry/exporters/otlp/otlp_http_log_record_exporter_options.h"
 #include "opentelemetry/exporters/otlp/otlp_environment.h"
 #include "opentelemetry/exporters/otlp/otlp_http.h"
+#include "opentelemetry/nostd/string_view.h"
 #include "opentelemetry/version.h"
 
 OPENTELEMETRY_BEGIN_NAMESPACE
@@ -20,6 +21,10 @@ OtlpHttpLogRecordExporterOptions::OtlpHttpLogRecordExporterOptions()
       console_debug(false),
       timeout(GetOtlpDefaultLogsTimeout()),
       http_headers(GetOtlpDefaultLogsHeaders()),
+#ifdef ENABLE_ASYNC_EXPORT
+      max_concurrent_requests{64},
+      max_requests_per_connection{8},
+#endif
       ssl_insecure_skip_verify(false),
       ssl_ca_cert_path(GetOtlpDefaultLogsSslCertificatePath()),
       ssl_ca_cert_string(GetOtlpDefaultLogsSslCertificateString()),
@@ -36,12 +41,7 @@ OtlpHttpLogRecordExporterOptions::OtlpHttpLogRecordExporterOptions()
       retry_policy_initial_backoff(GetOtlpDefaultLogsRetryInitialBackoff()),
       retry_policy_max_backoff(GetOtlpDefaultLogsRetryMaxBackoff()),
       retry_policy_backoff_multiplier(GetOtlpDefaultLogsRetryBackoffMultiplier())
-{
-#ifdef ENABLE_ASYNC_EXPORT
-  max_concurrent_requests     = 64;
-  max_requests_per_connection = 8;
-#endif
-}
+{}
 
 OtlpHttpLogRecordExporterOptions::~OtlpHttpLogRecordExporterOptions() {}
 
