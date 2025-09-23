80e7bb4fab664736608c4bd991cdc745f440c84e
Santiago Gimeno <santiago.gimeno@gmail.com>
2025-09-23 11:30:35 +0200

    QIP

--- deps/opentelemetry-cpp/exporters/otlp/src/otlp_metric_utils.cc
+++ deps/opentelemetry-cpp/exporters/otlp/src/otlp_metric_utils.cc
@@ -11,6 +11,7 @@
 #include "opentelemetry/exporters/otlp/otlp_metric_utils.h"
 #include "opentelemetry/exporters/otlp/otlp_populate_attribute_utils.h"
 #include "opentelemetry/exporters/otlp/otlp_preferred_temporality.h"
+#include "opentelemetry/nostd/string_view.h"
 #include "opentelemetry/nostd/variant.h"
 #include "opentelemetry/sdk/instrumentationscope/instrumentation_scope.h"
 #include "opentelemetry/sdk/metrics/data/circular_buffer.h"
@@ -112,7 +113,7 @@ void OtlpMetricUtils::ConvertSumMetric(const metric_sdk::MetricData &metric_data
     for (auto &kv_attr : point_data_with_attributes.attributes)
     {
       OtlpPopulateAttributeUtils::PopulateAttribute(proto_sum_point_data->add_attributes(),
-                                                    kv_attr.first, kv_attr.second);
+                                                    kv_attr.first, kv_attr.second, false);
     }
   }
 }
@@ -184,7 +185,7 @@ void OtlpMetricUtils::ConvertHistogramMetric(
     for (auto &kv_attr : point_data_with_attributes.attributes)
     {
       OtlpPopulateAttributeUtils::PopulateAttribute(proto_histogram_point_data->add_attributes(),
-                                                    kv_attr.first, kv_attr.second);
+                                                    kv_attr.first, kv_attr.second, false);
     }
   }
 }
@@ -248,7 +249,7 @@ void OtlpMetricUtils::ConvertExponentialHistogramMetric(
     for (auto &kv_attr : point_data_with_attributes.attributes)
     {
       OtlpPopulateAttributeUtils::PopulateAttribute(proto_histogram_point_data->add_attributes(),
-                                                    kv_attr.first, kv_attr.second);
+                                                    kv_attr.first, kv_attr.second, false);
     }
   }
 }
@@ -278,7 +279,7 @@ void OtlpMetricUtils::ConvertGaugeMetric(const opentelemetry::sdk::metrics::Metr
     for (auto &kv_attr : point_data_with_attributes.attributes)
     {
       OtlpPopulateAttributeUtils::PopulateAttribute(proto_gauge_point_data->add_attributes(),
-                                                    kv_attr.first, kv_attr.second);
+                                                    kv_attr.first, kv_attr.second, false);
     }
   }
 }
@@ -328,7 +329,7 @@ void OtlpMetricUtils::ConvertSummaryMetric(const metric_sdk::MetricData &metric_
     for (auto &kv_attr : point_data_with_attributes.attributes)
     {
       OtlpPopulateAttributeUtils::PopulateAttribute(proto_summary_point_data->add_attributes(),
-                                                    kv_attr.first, kv_attr.second);
+                                                    kv_attr.first, kv_attr.second, false);
     }
   }
 }
--- deps/opentelemetry-cpp/sdk/test/metrics/histogram_aggregation_benchmark.cc
+++ deps/opentelemetry-cpp/sdk/test/metrics/histogram_aggregation_benchmark.cc
@@ -3,19 +3,21 @@
 
 #include <benchmark/benchmark.h>
 #include <stddef.h>
-#include <algorithm>
 #include <chrono>
 #include <functional>
+#include <initializer_list>  // IWYU pragma: keep
 #include <random>
 #include <string>
 #include <thread>
 #include <utility>
 #include <vector>
 #include "common.h"
-#include "opentelemetry/context/context.h"
+
+#include "opentelemetry/context/context.h"  // IWYU pragma: keep
 #include "opentelemetry/metrics/meter.h"
 #include "opentelemetry/metrics/sync_instruments.h"
 #include "opentelemetry/nostd/shared_ptr.h"
+#include "opentelemetry/nostd/string_view.h"
 #include "opentelemetry/nostd/variant.h"
 #include "opentelemetry/sdk/instrumentationscope/instrumentation_scope.h"
 #include "opentelemetry/sdk/metrics/aggregation/aggregation_config.h"
