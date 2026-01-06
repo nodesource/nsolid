3b569e8b2ac813769c78f3c0091c6b47112c0570
Santiago Gimeno <santiago.gimeno@gmail.com>
2026-01-06 10:22:22 +0100

    test: add grpc tests testing TLS connections
    Only grpc insecure connections were being used in the tests. Also, these
    new tests allow to cover for the untested `NSOLID_GRPC_CERTS` env
    variable.
    
    PR-URL: https://github.com/nodesource/nsolid/pull/406
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- test/common/nsolid-grpc-agent/index.js
+++ test/common/nsolid-grpc-agent/index.js
@@ -68,18 +68,24 @@ function checkResource(resource, agentId, config, metrics) {
 
 
 class GRPCServer extends EventEmitter {
+  #opts;
   #server;
-  constructor() {
+  constructor(opts) {
     super();
     this.#server = null;
+    this.#opts = opts || {};
   }
 
   start(cb) {
     const args = [];
+    if (this.#opts.tls) {
+      args.push('--tls');
+    }
+
     const opts = {
       stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
     };
-    this.#server = fork(path.join(__dirname, 'server.mjs') + '', args, opts);
+    this.#server = fork(path.join(__dirname, 'server.mjs'), args, opts);
     this.#server.on('message', (message) => {
       console.log('message', message);
       switch (message.type) {
