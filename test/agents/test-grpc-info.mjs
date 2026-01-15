f46c610750662afb243795e7d56cda973e0fea24
Santiago Gimeno <santiago.gimeno@gmail.com>
2026-01-15 18:57:27 +0100

    agents: on grpc add appVersion field to info msg
    It wasn't added when creating the gRPC agent. Improved tests to cover
    this specific issue so it doesn't happen again.

--- test/agents/test-grpc-info.mjs
+++ test/agents/test-grpc-info.mjs
@@ -1,140 +1,166 @@
 // Flags: --expose-internals
-import { mustSucceed } from '../common/index.mjs';
+import { mustCall, mustSucceed } from '../common/index.mjs';
+import fixtures from '../common/fixtures.js';
 import assert from 'node:assert';
-import { fork } from 'node:child_process';
-import { fileURLToPath } from 'node:url';
-import { isMainThread, Worker } from 'node:worker_threads';
-import nsolid from 'nsolid';
 import {
   GRPCServer,
+  TestClient,
 } from '../common/nsolid-grpc-agent/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
+function checkInfoData(msg, metadata, requestId, agentId, nsolidConfig = {}) {
+  const info = msg;
+  assert.strictEqual(info.common.requestId, requestId);
+  assert.strictEqual(info.common.command, 'info');
+  assert.strictEqual(info.common.error, null);
+  assert.strictEqual(info.body.id, agentId);
+  assert.ok(info.common.recorded);
+  assert.ok(info.common.recorded.seconds);
+  assert.ok(info.common.recorded.nanoseconds);
+  // also the body fields
+  assert.ok(info.body.app);
+  assert.strictEqual(info.body.app, nsolidConfig.appName || 'untitled application');
+  assert.strictEqual(info.body.appVersion, nsolidConfig.appVersion);
+  assert.ok(info.body.arch);
+  assert.ok(info.body.cpuCores);
+  assert.ok(info.body.cpuModel);
+  assert.ok(info.body.execPath);
+  assert.ok(info.body.hostname);
+  assert.ok(info.body.id);
+  assert.ok(info.body.main);
+  assert.strictEqual(info.body.nodeEnv, nsolidConfig.nodeEnv || 'prod');
+  assert.ok(info.body.pid);
+  assert.ok(info.body.platform);
+  assert.ok(info.body.processStart);
+  assert.strictEqual(info.body.tags.length, nsolidConfig.tags ? nsolidConfig.tags.length : 0);
+  assert.ok(info.body.totalMem);
+  assert.deepStrictEqual(info.body.versions, process.versions);
+  assert.strictEqual(typeof info.body.kernelVersion, 'number');
+  assert.ok(metadata['user-agent']);
+  assert.ok(metadata['nsolid-agent-id']);
+  assert.strictEqual(metadata['nsolid-agent-id'][0], agentId);
+}
 
-if (process.argv[2] === 'child') {
-  // Just to keep the worker alive.
-  setInterval(() => {
-  }, 1000);
+const tests = [];
 
-  if (isMainThread) {
-    nsolid.start({
-      tracingEnabled: false,
-    });
-
-    const worker = new Worker(__filename, { argv: ['child'] });
-    process.send({ type: 'workerThreadId', id: worker.threadId });
-    process.send({
-      type: 'nsolid',
-      id: nsolid.id,
-      appName: nsolid.appName,
-    });
-    process.on('message', (message) => {
-      assert.strictEqual(message, 'exit');
-      process.exit(0);
-    });
-  }
-} else {
-  function checkInfoData(msg, metadata, requestId, agentId, nsolidConfig = {}) {
-    const info = msg;
-    assert.strictEqual(info.common.requestId, requestId);
-    assert.strictEqual(info.common.command, 'info');
-    assert.strictEqual(info.common.error, null);
-    assert.strictEqual(info.body.id, agentId);
-    assert.ok(info.common.recorded);
-    assert.ok(info.common.recorded.seconds);
-    assert.ok(info.common.recorded.nanoseconds);
-    // also the body fields
-    assert.ok(info.body.app);
-    assert.strictEqual(info.body.app, nsolidConfig.appName || 'untitled application');
-    assert.ok(info.body.arch);
-    assert.ok(info.body.cpuCores);
-    assert.ok(info.body.cpuModel);
-    assert.ok(info.body.execPath);
-    assert.ok(info.body.hostname);
-    assert.ok(info.body.id);
-    assert.ok(info.body.main);
-    assert.strictEqual(info.body.nodeEnv, nsolidConfig.nodeEnv || 'prod');
-    assert.ok(info.body.pid);
-    assert.ok(info.body.platform);
-    assert.ok(info.body.processStart);
-    assert.strictEqual(info.body.tags.length, nsolidConfig.tags ? nsolidConfig.tags.length : 0);
-    assert.ok(info.body.totalMem);
-    assert.deepStrictEqual(info.body.versions, process.versions);
-    assert.strictEqual(typeof info.body.kernelVersion, 'number');
-    assert.ok(metadata['user-agent']);
-    assert.ok(metadata['nsolid-agent-id']);
-    assert.strictEqual(metadata['nsolid-agent-id'][0], agentId);
-  }
-
-  async function runTest({ getEnv, nsolidConfig }) {
-    return new Promise((resolve, reject) => {
-      const grpcServer = new GRPCServer();
+tests.push({
+  name: 'should retrieve info with default config',
+  test: async (getEnv, isSecure) => {
+    return new Promise((resolve) => {
+      const grpcServer = new GRPCServer({ tls: isSecure });
       grpcServer.start(mustSucceed(async (port) => {
-        console.log('GRPC server started', port);
-        const env = getEnv(port);
+        const env = getEnv(port, isSecure);
         const opts = {
           stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
           env,
         };
-        const child = fork(__filename, ['child'], opts);
-        grpcServer.on('command', async ({ agentId }) => {
+
+        const nsolidConfig = {
+          appName: 'nsolid-grpc-agent',
+          appVersion: '1.0.0',
+          tags: [],
+          nodeEnv: 'prod',
+        };
+
+        grpcServer.on('command', mustCall(async ({ agentId }) => {
           const { data, requestId } = await grpcServer.info(agentId);
           checkInfoData(data.msg, data.metadata, requestId, agentId, nsolidConfig);
           console.dir(data, { depth: null });
-          child.send('exit');
-        });
-
-        child.on('exit', (code, signal) => {
-          console.log(`child process exited with code ${code} and signal ${signal}`);
+          await child.shutdown(0);
           grpcServer.close();
           resolve();
-        });
+        }));
+
+        const child = new TestClient([], opts);
       }));
     });
-  }
+  },
+});
 
-  const testConfigs = [
-    {
-      getEnv: (port) => {
-        return {
-          NODE_DEBUG_NATIVE: 'nsolid_grpc_agent',
-          NSOLID_GRPC_INSECURE: 1,
-          NSOLID_GRPC: `localhost:${port}`,
-        };
-      },
-      nsolidConfig: {},
-    },
-    {
-      getEnv: (port) => {
-        return {
-          NODE_DEBUG_NATIVE: 'nsolid_grpc_agent',
-          NSOLID_GRPC_INSECURE: 1,
-          NSOLID_SAAS: `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbtesting.localhost:${port}`,
-        };
-      },
-      nsolidConfig: {},
-    },
-    {
-      getEnv: (port) => {
-        return {
-          NODE_DEBUG_NATIVE: 'nsolid_grpc_agent',
-          NSOLID_GRPC_INSECURE: 1,
-          NSOLID_GRPC: `localhost:${port}`,
+tests.push({
+  name: 'should retrieve info with custom config',
+  test: async (getEnv, isSecure) => {
+    return new Promise((resolve) => {
+      const grpcServer = new GRPCServer({ tls: isSecure });
+      grpcServer.start(mustSucceed(async (port) => {
+        const env = getEnv(port, isSecure, {
           NSOLID_APPNAME: 'myapp',
           NSOLID_TAGS: 'tag1,tag2',
           NODE_ENV: 'dev',
-        };
-      },
-      nsolidConfig: {
-        appName: 'myapp',
-        tags: ['tag1', 'tag2'],
-        nodeEnv: 'dev',
-      },
-    },
-  ];
+        });
 
-  for (const testConfig of testConfigs) {
-    await runTest(testConfig);
-    console.log('run test!');
+        const opts = {
+          stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+          env,
+        };
+
+        const nsolidConfig = {
+          appName: 'myapp',
+          appVersion: '1.0.0',
+          tags: ['tag1', 'tag2'],
+          nodeEnv: 'dev',
+        };
+
+        grpcServer.on('command', mustCall(async ({ agentId }) => {
+          const { data, requestId } = await grpcServer.info(agentId);
+          checkInfoData(data.msg, data.metadata, requestId, agentId, nsolidConfig);
+          console.dir(data, { depth: null });
+          await child.shutdown(0);
+          grpcServer.close();
+          resolve();
+        }));
+
+        const child = new TestClient([], opts);
+      }));
+    });
+  },
+});
+
+const testConfigs = [
+  {
+    getEnv: (port, isSecure, extraEnv = {}) => {
+      const env = {
+        NODE_DEBUG_NATIVE: 'nsolid_grpc_agent',
+        NSOLID_GRPC: `localhost:${port}`,
+        ...extraEnv,
+      };
+      if (!isSecure) {
+        env.NSOLID_GRPC_INSECURE = 1;
+      } else {
+        env.NSOLID_GRPC_CERTS = fixtures.path('keys', 'selfsigned-no-keycertsign', 'cert.pem');
+      }
+      return env;
+    },
+    nsolidConfig: {
+      appName: 'nsolid-grpc-agent',
+      appVersion: '1.0.0',
+      tags: [],
+      nodeEnv: 'prod',
+    },
+  },
+  {
+    getEnv: (port, isSecure, extraEnv = {}) => {
+      const env = {
+        NODE_DEBUG_NATIVE: 'nsolid_grpc_agent',
+        NSOLID_SAAS: `aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbbbbbbbbbbbbtesting.localhost:${port}`,
+        ...extraEnv,
+      };
+      if (!isSecure) {
+        env.NSOLID_GRPC_INSECURE = 1;
+      } else {
+        env.NSOLID_GRPC_CERTS = fixtures.path('keys', 'selfsigned-no-keycertsign', 'cert.pem');
+      }
+      return env;
+    },
+    nsolidConfig: {},
+  },
+];
+
+const isSecureOpts = [false, true];
+for (const testConfig of testConfigs) {
+  for (const { name, test } of tests) {
+    for (const isSecure of isSecureOpts) {
+      console.log(`[info] ${name} ${isSecure ? 'secure' : 'insecure'}`);
+      await test(testConfig.getEnv, isSecure);
+    }
   }
 }
