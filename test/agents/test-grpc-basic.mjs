e6447e79406dd3f512af35be0c429fb288cdf43a
Santiago Gimeno <santiago.gimeno@gmail.com>
2026-01-15 16:04:14 +0100

    agents: fix infinite recursion on ~CommandStream
    When trying to reset a CommandStream because of reasons: for example, we
    change `grpc` endpoint dynamically, we can enter an infinite recursion
    calling `GrpcAgent::reset_command_stream()` because when calling
    `~CommandStream()` the `OnDone()` callback may call the observer
    (GrpcAgent instance) which in turn would call
    `GrpcAgent::reset_command_stream()`. Avoid this recursion by adding a
    new `cancelling_for_destruction_` member variable to `CommandStream`
    which allows avoiding calling the observer if triggered by the
    destructor.
    
    PR-URL: https://github.com/nodesource/nsolid/pull/412
    Reviewed-By: EHortua <55801532+EHortua@users.noreply.github.com>

--- test/agents/test-grpc-basic.mjs
+++ test/agents/test-grpc-basic.mjs
@@ -126,6 +126,61 @@ tests.push({
   },
 });
 
+tests.push({
+  name: 'should reconnect after initial invalid NSOLID_GRPC',
+  test: async (getEnv, isSecure) => {
+    return new Promise((resolve) => {
+      const grpcServer = new GRPCServer({ tls: isSecure });
+      grpcServer.start(mustSucceed(async (port) => {
+        grpcServer.on('exit', mustCall((data) => {
+          checkExitData(data.msg, data.metadata, agentId, { code: 0, error: null, profile: '' });
+          grpcServer.close();
+          resolve();
+        }));
+
+        const correctEnv = getEnv(port, isSecure);
+        const env = {
+          NODE_DEBUG_NATIVE: 'nsolid_grpc_agent',
+          NSOLID_GRPC: '127.0.0.1:1',
+        };
+        if (correctEnv.NSOLID_GRPC_INSECURE) {
+          env.NSOLID_GRPC_INSECURE = correctEnv.NSOLID_GRPC_INSECURE;
+        }
+        if (correctEnv.NSOLID_GRPC_CERTS) {
+          env.NSOLID_GRPC_CERTS = correctEnv.NSOLID_GRPC_CERTS;
+        }
+
+        const opts = {
+          stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+          env,
+        };
+        const child = new TestClient([], opts);
+        const agentId = await child.id();
+
+        const config = {};
+        if (correctEnv.NSOLID_GRPC) {
+          config.grpc = correctEnv.NSOLID_GRPC;
+        }
+        if (correctEnv.NSOLID_SAAS) {
+          config.saas = correctEnv.NSOLID_SAAS;
+        }
+
+        grpcServer.on('command', async ({ agentId }) => {
+          // Verify the CommandStream is working by sending a command from server to client
+          const infoResult = await grpcServer.info(agentId);
+          assert.ok(infoResult);
+          const exit = await child.shutdown(0);
+          assert.ok(exit);
+          assert.strictEqual(exit.code, 0);
+          assert.strictEqual(exit.signal, null);
+        });
+
+        await child.config(config);
+      }));
+    });
+  },
+});
+
 const testConfigs = [
   {
     getEnv: (port, isSecure) => {
