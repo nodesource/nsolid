d726aa46a01b96784ba176503b8135601baf2031
Santiago Gimeno <santiago.gimeno@gmail.com>
2026-09-21 17:52:41 +0200

    vex: add CVE-2026-48758 CVE-2026-48815 exceptions
    Signed-off-by: Santiago Gimeno <santiago.gimeno@gmail.com>
    PR-URL: https://github.com/nodesource/nsolid/pull/526
    Reviewed-By: Rafael Gonzaga <rafael.nunu@hotmail.com>

--- test/tools/test_vex.py
+++ test/tools/test_vex.py
@@ -25,6 +25,20 @@ class GenerateVEXTest(unittest.TestCase):
       generate_document([{'cve': 'CVE-2099-0001', 'justification':
                           'vulnerable_code_not_present', 'product': 'nsolid'}])
 
+  def test_product_with_subcomponents(self):
+    document = generate_document([{
+        'cve': 'CVE-2099-0002',
+        'status': 'fixed',
+        'action_statement': 'Backported upstream fix.',
+        'product': {
+            '@id': 'pkg:generic/foo@1.0.0',
+            'subcomponents': [{'@id': 'pkg:npm/bar@2.0.0'}],
+        },
+    }], '2026-01-01T00:00:00Z')
+    product = document['statements'][0]['products'][0]
+    self.assertEqual(product['@id'], 'pkg:generic/foo@1.0.0')
+    self.assertEqual(product['subcomponents'][0]['@id'], 'pkg:npm/bar@2.0.0')
+
   def test_invalid_impact_statement(self):
     with self.assertRaisesRegex(ValueError, 'Invalid impact statement'):
       generate_document([{'cve': 'CVE-2099-0001', 'justification':
@@ -41,6 +55,27 @@ class GenerateVEXTest(unittest.TestCase):
     self.assertEqual(statement['vulnerability']['name'], 'CVE-2099-0001')
     self.assertEqual(statement['justification'], 'vulnerable_code_not_present')
     self.assertEqual(statement['impact_statement'], 'Test-only exception.')
+
+  def test_fixed_exception(self):
+    document = generate_document([{
+        'cve': 'CVE-2099-0002',
+        'status': 'fixed',
+        'action_statement': 'Backported upstream fix.',
+    }], '2026-01-01T00:00:00Z')
+    statement = document['statements'][0]
+    self.assertEqual(statement['status'], 'fixed')
+    self.assertEqual(statement['vulnerability']['name'], 'CVE-2099-0002')
+    self.assertEqual(statement['action_statement'], 'Backported upstream fix.')
+    self.assertNotIn('justification', statement)
+
+  def test_fixed_with_justification_error(self):
+    with self.assertRaisesRegex(ValueError, 'Fixed statements must not include justification'):
+      generate_document([{
+          'cve': 'CVE-2099-0003',
+          'status': 'fixed',
+          'justification': 'vulnerable_code_not_present',
+      }])
+
 
 
 if __name__ == '__main__':
